502fda6c18bb3cee7c65953034e37cab01e3d9b3
sihyeon <ehpoos92@hanmail.net>
2017-01-15 16:11:50 +0900

    mainschedule fragment 입력 부분 구현

--- app/src/main/java/com/team/codealmanac/w2do/fragment/TodoSimpleListFragment.java
+++ app/src/main/java/com/team/codealmanac/w2do/fragment/TodoSimpleListFragment.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.team.codealmanac.w2do.R;
@@ -15,6 +19,12 @@ import com.team.codealmanac.w2do.R;
 public class TodoSimpleListFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+
+    private FrameLayout main_schedule_framelayout;
+    private TextView main_schedule_header;
+    private TextView main_schedule_sec_header;
+    private EditText main_schedule_edittext;
+    private Button mschedule_input_btn;
 
     public TodoSimpleListFragment() {
         // Required empty public constructor
@@ -38,9 +48,35 @@ public class TodoSimpleListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_todo_simple_list, container, false);
-        TextView textView = (TextView)view.findViewById(R.id.frag_main_schedule_header_text);
-        textView.setVisibility(View.VISIBLE);
+
+        main_schedule_framelayout = (FrameLayout)view.findViewById(R.id.main_schedule_framelayout);
+        main_schedule_header = (TextView)view.findViewById(R.id.frag_main_schedule_header_text);
+        main_schedule_sec_header = (TextView)view.findViewById(R.id.frag_maininput_msg);
+        main_schedule_edittext = (EditText)view.findViewById(R.id.main_schedule_input);
+        mschedule_input_btn = (Button)view.findViewById(R.id.mainschedule_input_btn);
+
+        main_schedule_header.setVisibility(View.VISIBLE);
+        main_schedule_sec_header.setVisibility(View.VISIBLE);
+        main_schedule_edittext.setVisibility(View.VISIBLE);
+        mschedule_input_btn.setVisibility(View.VISIBLE);
+        mschedule_input_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String main_schedule = main_schedule_edittext.getText().toString();
+                if(TextUtils.isEmpty(main_schedule)){
+                    main_schedule_edittext.setError("메인 스케줄을 입력해주세요.");
+                    return;
+                }
+            }
+        });
+
         return view;
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -53,12 +89,6 @@ public class TodoSimpleListFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
     }
 
     @Override
@@ -72,4 +102,5 @@ public class TodoSimpleListFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
 }
