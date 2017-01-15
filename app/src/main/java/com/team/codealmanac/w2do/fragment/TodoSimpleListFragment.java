4f818c732bce2378defb80207846345140d187c2
sihyeon <ehpoos92@hanmail.net>
2017-01-15 17:18:55 +0900

    mainschedule fragment frame 완료 // (adapter,db 연결 남음)

--- app/src/main/java/com/team/codealmanac/w2do/fragment/TodoSimpleListFragment.java
+++ app/src/main/java/com/team/codealmanac/w2do/fragment/TodoSimpleListFragment.java
@@ -1,9 +1,12 @@
 package com.team.codealmanac.w2do.fragment;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +16,10 @@ import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
 import com.team.codealmanac.w2do.R;
+import com.team.codealmanac.w2do.adapter.AddRemoveNumberedAdapter;
+import com.team.codealmanac.w2do.models.MainSchedule;
 
 
 public class TodoSimpleListFragment extends Fragment {
@@ -21,10 +27,12 @@ public class TodoSimpleListFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
 
     private FrameLayout main_schedule_framelayout;
+    private RecyclerView today_listview;
     private TextView main_schedule_header;
     private TextView main_schedule_sec_header;
     private EditText main_schedule_edittext;
     private Button mschedule_input_btn;
+    private DatabaseReference mDatabase;
 
     public TodoSimpleListFragment() {
         // Required empty public constructor
@@ -54,10 +62,12 @@ public class TodoSimpleListFragment extends Fragment {
         main_schedule_sec_header = (TextView)view.findViewById(R.id.frag_maininput_msg);
         main_schedule_edittext = (EditText)view.findViewById(R.id.main_schedule_input);
         mschedule_input_btn = (Button)view.findViewById(R.id.mainschedule_input_btn);
+        today_listview = (RecyclerView)view.findViewById(R.id.today_listview);
 
         main_schedule_header.setVisibility(View.VISIBLE);
         main_schedule_sec_header.setVisibility(View.VISIBLE);
         main_schedule_edittext.setVisibility(View.VISIBLE);
+        today_listview.setVisibility(View.VISIBLE);
         mschedule_input_btn.setVisibility(View.VISIBLE);
         mschedule_input_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,6 +77,8 @@ public class TodoSimpleListFragment extends Fragment {
                     main_schedule_edittext.setError("메인 스케줄을 입력해주세요.");
                     return;
                 }
+                // 메인 스케줄 db 연결하세요~
+                writeMainSchedule(main_schedule);
             }
         });
 
@@ -76,6 +88,11 @@ public class TodoSimpleListFragment extends Fragment {
     @Override
     public void onStart(){
         super.onStart();
+
+    }
+
+    // 메인 스케줄 데이터 write 함수
+    private void writeMainSchedule(String mainschedule){
 
     }
 
