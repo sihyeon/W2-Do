3bf3bf26ed13c0fe3de43fcdcaa8f2c79c7fbf97
sihyeon <ehpoos92@hanmail.net>
2017-01-17 23:49:55 +0900

    custom checkbox 완료

--- app/src/main/java/com/team/codealmanac/w2do/fragment/CalendarFragment.java
+++ app/src/main/java/com/team/codealmanac/w2do/fragment/CalendarFragment.java
@@ -33,7 +33,7 @@ public class CalendarFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_navitem_calender, container, false);
+        View view = inflater.inflate(R.layout.item_checkbox_layout, container, false);
 
 
         return view;
