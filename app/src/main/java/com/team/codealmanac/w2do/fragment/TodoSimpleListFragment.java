7d4f7e878b995919f383a01f4758e3ed97b2ce60
sihyeon <ehpoos92@hanmail.net>
2017-01-18 19:08:33 +0900

    TodosimpleListFragment 디자인 가이드 적용 및 플로팅 버튼 custom action behavior 구현

--- app/src/main/java/com/team/codealmanac/w2do/fragment/TodoSimpleListFragment.java
+++ app/src/main/java/com/team/codealmanac/w2do/fragment/TodoSimpleListFragment.java
@@ -5,6 +5,7 @@ import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
@@ -45,6 +46,8 @@ public class TodoSimpleListFragment extends Fragment {
     private String mUserId;
     private DatabaseReference mMainScheduleReference;
     private ChildEventListener mMainScheduleListener;
+
+
     public TodoSimpleListFragment() {
         // Required empty public constructor
     }
