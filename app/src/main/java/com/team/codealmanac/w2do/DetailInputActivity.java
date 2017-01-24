d26526951f84092b9215172e239870d8badf69d9
sihyeon <ehpoos92@hanmail.net>
2017-01-24 18:17:53 +0900

    상세페이지 frame 완료

--- app/src/main/java/com/team/codealmanac/w2do/DetailInputActivity.java
+++ app/src/main/java/com/team/codealmanac/w2do/DetailInputActivity.java
@@ -4,6 +4,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
@@ -15,40 +17,45 @@ import android.widget.TextView;
  * Created by sihyeon on 2017-01-15.
  */
 
-public class DetailInputActivity extends AppCompatActivity{
-
-    private TextView act_detailInout_folder_select;
+public class DetailInputActivity extends AppCompatActivity {
     private Spinner act_detailInput_folder_spinner;
-    private Toolbar act_detailInput_toolbar;
-    private TextView act_detailInput_date_select;
-    private EditText act_detailInput_editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-        }
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detailinput);
-
-        // toolbar 설정
-        act_detailInput_toolbar = (Toolbar) findViewById(R.id.act_detailInput_toolbar);
-        setSupportActionBar(act_detailInput_toolbar);
+        setContentView(R.layout.activity_detail_input);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        act_detailInput_folder_spinner = (Spinner)findViewById(R.id.act_detailInput_folder_spinner);
 
         String[] Folder_Spinner_item = getResources().getStringArray(R.array.folder);
         ArrayAdapter<String> Folder_Spinner_Adapter = new ArrayAdapter<String>(
                 this, R.layout.activity_detailinput_folder_spinner_textview, Folder_Spinner_item);
-
-        act_detailInout_folder_select = (TextView) findViewById(R.id.act_detailInput_folder_select);
-        act_detailInput_folder_spinner = (Spinner) findViewById(R.id.act_detailInput_folder_select_spinner);
-        act_detailInput_date_select = (TextView) findViewById(R.id.act_detailInput_date_start);
-        act_detailInput_editText = (EditText) findViewById(R.id.act_detailInput_editText);
-
         Folder_Spinner_Adapter.setDropDownViewResource(R.layout.activity_detailinput_folder_spinner_textview);
-        act_detailInput_folder_spinner.setAdapter(Folder_Spinner_Adapter);
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_detail_input, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_submit_btn) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
