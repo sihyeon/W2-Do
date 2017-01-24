f4f9ad7dc3d1398fcdc50b57cd324a67eeffc557
sihyeon <ehpoos92@hanmail.net>
2017-01-24 18:49:08 +0900

    상세페이지 기능 구현 작업 중 -> 파일명,변수명 refactor

--- app/src/main/java/com/team/codealmanac/w2do/DetailInputActivity.java
+++ app/src/main/java/com/team/codealmanac/w2do/DetailInputActivity.java
@@ -3,22 +3,44 @@ package com.team.codealmanac.w2do;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-/**
- * Created by sihyeon on 2017-01-15.
- */
-
 public class DetailInputActivity extends AppCompatActivity {
+    // 첫 번째 cardview items : 폴더 선택,내용입력,색상 설정
+    private CardView act_detailInput_title_cardview;
     private Spinner act_detailInput_folder_spinner;
+    private TextView act_detailInput_title_text;
+    private Button act_detailInput_color_picker;
+    private EditText act_detailInput_content_edittext;
+    private TextView act_detailInput_folder_text;
+
+    // 두 번째 cardview items : 캘린더,시간설정
+    private CardView act_detailInput_calendar_cardview;
+    private TextView act_detailInput_calendar_start_text;
+    private TextView act_detailInput_calendar_end_text;
+    private TextView act_detailInput_start_date_display;
+    private TextView act_detailInput_end_date_display;
+    private Button act_detailInput_calendar_allday_btn;
+    private TextView act_detailInput_start_time_display;
+    private TextView act_detailInput_end_time_display;
+
+    // 세 번째 cardview items: more detail buttons
+    private CardView act_detailInput_more_detail_btn_cardview;
+    private TextView act_detailInput_more_detail_title_text;
+    private Button act_detailInput_more_detail_side_btn_gps;
+    private Button act_detailInput_more_detail_side_btn_alarm;
+    private Button act_detailInput_more_detail_side_btn_memo;
+    private Button act_detailInput_more_detail_side_btn_share;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +50,38 @@ public class DetailInputActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        // first cardview items
+        act_detailInput_title_cardview = (CardView)findViewById(R.id.act_detailInput_title_cardview);
+        act_detailInput_title_text = (TextView)findViewById(R.id.act_detailInput_title_text);
+        act_detailInput_color_picker = (Button)findViewById(R.id.act_detailInput_color_picker);
+        act_detailInput_content_edittext = (EditText)findViewById(R.id.act_detailInput_content_edittext);
+        act_detailInput_folder_text = (TextView)findViewById(R.id.act_detailInput_folder_text);
         act_detailInput_folder_spinner = (Spinner)findViewById(R.id.act_detailInput_folder_spinner);
+
+        // second cardview items
+        act_detailInput_calendar_cardview = (CardView)findViewById(R.id.act_detailInput_calendar_cardview);
+        act_detailInput_calendar_start_text = (TextView)findViewById(R.id.act_detailInput_calendar_start_text);
+        act_detailInput_calendar_end_text = (TextView)findViewById(R.id.act_detailInput_calendar_end_text);
+        act_detailInput_start_date_display = (TextView)findViewById(R.id.act_detailInput_start_date_display);
+        act_detailInput_end_date_display = (TextView)findViewById(R.id.act_detailInput_end_date_display);
+        act_detailInput_start_time_display = (TextView)findViewById(R.id.act_detailInput_start_time_display);
+        act_detailInput_end_time_display = (TextView)findViewById(R.id.act_detailInput_end_time_display);
+        act_detailInput_calendar_allday_btn = (Button)findViewById(R.id.act_detailInput_calendar_allday_btn);
+
+        //third cardview items
+        act_detailInput_more_detail_btn_cardview = (CardView)findViewById(R.id.act_detailInput_more_detail_btn_cardview);
+        act_detailInput_more_detail_title_text = (TextView)findViewById(R.id.act_detailInput_more_detail_title_text);
+        act_detailInput_more_detail_side_btn_gps = (Button)findViewById(R.id.act_detailInput_more_detail_side_btn_gps);
+        act_detailInput_more_detail_side_btn_alarm = (Button)findViewById(R.id.act_detailInput_more_detail_side_btn_alarm);
+        act_detailInput_more_detail_side_btn_memo = (Button)findViewById(R.id.act_detailInput_more_detail_side_btn_memo);
+        act_detailInput_more_detail_side_btn_gps = (Button)findViewById(R.id.act_detailInput_more_detail_side_btn_gps);
+        act_detailInput_more_detail_side_btn_share = (Button)findViewById(R.id.act_detailInput_more_detail_side_btn_share);
 
         String[] Folder_Spinner_item = getResources().getStringArray(R.array.folder);
         ArrayAdapter<String> Folder_Spinner_Adapter = new ArrayAdapter<String>(
                 this, R.layout.activity_detailinput_folder_spinner_textview, Folder_Spinner_item);
         Folder_Spinner_Adapter.setDropDownViewResource(R.layout.activity_detailinput_folder_spinner_textview);
+
     }
 
     @Override
